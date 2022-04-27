8f483cf2a36b4cf07ec7dc5d77274d9f97240e27
Benny <73031058+Qin-Yuan@users.noreply.github.com>
2022-04-27 20:11:24 +0800

    Update MPU6050.ino

--- Arduino_平衡小车/4_MPU6050陀螺仪/MPU6050/MPU6050.ino
+++ Arduino_平衡小车/4_MPU6050陀螺仪/MPU6050/MPU6050.ino
@@ -9,6 +9,8 @@
  *             GND        GND
  * 问题：采集的数据干扰较大，下一节使用卡尔曼滤波处理数据
 **************************************************************/
+// Github下载链接
+// https://github.com/Qin-Yuan/Arduino/tree/main/Arduino_%E5%B9%B3%E8%A1%A1%E5%B0%8F%E8%BD%A6/4_MPU6050%E9%99%80%E8%9E%BA%E4%BB%AA/MPU6050
 
 #include "./I2Cdev.h"             // I2C通信获取MPU6050数据
 #include "./MPU6050_6Axis_MotionApps20.h"  // 6050陀螺仪控制库
@@ -33,12 +35,12 @@ void setup() {
 void loop() {
     mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);     // IIC获取MPU6050六轴数据 ax ay az gx gy gz
     // 输出陀螺仪的数据
-    Serial.print("x轴角度: "); Serial.println(ax);
-    Serial.print("y轴角度: "); Serial.println(ay);
-    Serial.print("z轴角度: "); Serial.println(az);
-    Serial.print("x轴角速度: "); Serial.println(gx);
-    Serial.print("y轴角速度: "); Serial.println(gy);
-    Serial.print("z轴角速度: "); Serial.println(gz);
+    Serial.print("x轴加速度: "); Serial.println(ax);
+    Serial.print("y轴加速度: "); Serial.println(ay);
+    Serial.print("z轴加速度: "); Serial.println(az);
+    Serial.print("x轴角度: "); Serial.println(gx);
+    Serial.print("y轴角度: "); Serial.println(gy);
+    Serial.print("z轴角度: "); Serial.println(gz);
     Serial.println();
     delay(1500);
 }
